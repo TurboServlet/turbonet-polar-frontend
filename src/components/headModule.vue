46561b1acee1231dde3b86627d2ea70d4d963407
YuuLuo <69673808+YuuLuo@users.noreply.github.com>
2025-04-18 10:15:36 +0800

    Add i18n support for Arabic language

--- src/components/headModule.vue
+++ src/components/headModule.vue
@@ -75,7 +75,7 @@ const changeLang = (lang) => {
             </button>
           </li>
           <li>
-            <button @click="changeLang('ru')" :class="{ 'menu-active': locale === 'ar' }">
+            <button @click="changeLang('ar')" :class="{ 'menu-active': locale === 'ar' }">
               <span class="pe-4 font-mono text-[.5625rem] font-bold tracking-[0.09375rem] opacity-40">AR</span>
               <span class="font-[sans-serif]">اللغة العربية</span>
             </button>
